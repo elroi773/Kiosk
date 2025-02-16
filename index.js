76aa76817bed7125c4b084855757aba1987ef96d
name <s2457@e-mirim.hs.kr>
2025-02-16 20:32:58 +0900

    주문 유형에서 막힘 ㅠ

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const togoButton = document.getElementById("togo");
     const tohereButton = document.getElementById("tohere");
-    
-    togoButton.addEventListener("click", function() {
+
+    togoButton.addEventListener("click", function () {
         navigateToOrder("포장");
     });
-    
-    tohereButton.addEventListener("click", function() {
-        navigateToOrder("매장" );
+
+    tohereButton.addEventListener("click", function () {
+        navigateToOrder("매장");
     });
 });
 
 function navigateToOrder(type) {
-    window.location.href = `main.html?orderType=${type}`;
+    window.open(`main.html?orderType=${type}`, '_blank');
+    window.open(`nonecoffee.html?orderType=${type}`, '_blank');
 }
